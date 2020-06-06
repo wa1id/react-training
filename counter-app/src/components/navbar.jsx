7a59b20fa950b86b9a1f04479246f69c634e138e
wa1id <wa1id@users.noreply.github.com>
2020-06-07 00:14:45 +0200

    Add NavBar

---
+++ counter-app/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavbBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavbBar;
