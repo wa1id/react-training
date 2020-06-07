cfb9cafc7e799054bddcf296d118b62ce529d4df
wa1id <wa1id@users.noreply.github.com>
2020-06-07 20:53:58 +0200

    Add bootstrap grid

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,20 +3,26 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Button
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
+      <div className="container">
+        <div className="row">
+          <div className="col-1">
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          </div>
+          <div className="col-2">
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="btn btn-secondary btn-sm"
+            >
+              +
+            </button>
+            <button
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-sm m-2"
+            >
+              X
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
