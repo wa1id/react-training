dedbe1f6840f2dd2770bd85db2151eaf7eef5dd2
wa1id <wa1id@users.noreply.github.com>
2020-06-07 21:56:06 +0200

    stateless functional component

--- vidly/src/components/common/like.jsx
+++ vidly/src/components/common/like.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        className={classes}
-        aria-hidden="true"
-      ></i>
-    );
-  }
-}
-
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
 export default Like;
