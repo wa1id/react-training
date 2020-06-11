52e8491cb7007ed0d1f61b1a4f2e6698a028816a
wa1id <wa1id@users.noreply.github.com>
2020-06-11 21:10:23 +0200

    onClick on tableheader

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -41,6 +41,10 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
+  handleSort = (path) => {
+    console.log(path);
+  };
+
   render() {
     const { length: count } = this.state.movies;
     const {
@@ -79,6 +83,7 @@ class Movies extends Component {
             movies={movies}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
