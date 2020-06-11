cd57a998585a4bba0d170b71b3f949884f4d6d57
wa1id <wa1id@users.noreply.github.com>
2020-06-11 19:42:41 +0200

    hide pagination if only 1 page

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -3,7 +3,9 @@ import _ from "lodash";
 
 const Pagination = (props) => {
   const { itemsCount, pageSize } = props;
-  const pagesCount = itemsCount / pageSize;
+  const pagesCount = Math.ceil(itemsCount / pageSize); // afronden
+
+  if (pagesCount === 1) return null; // don't render anything if there is only 1 page
 
   const pages = _.range(1, pagesCount + 1);
 
