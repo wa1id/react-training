aeb73601705a1e8ee5fd055aab2fd10b339c63fd
wa1id <wa1id@users.noreply.github.com>
2020-06-13 19:05:26 +0200

    show pointer when hovering over clolumn

--- vidly/src/components/common/tableHeader.jsx
+++ vidly/src/components/common/tableHeader.jsx
@@ -30,6 +30,7 @@ class TableHeader extends Component {
         <tr>
           {this.props.columns.map((column) => (
             <th
+              className="clickable"
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
