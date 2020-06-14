de6413a037b38cce77a6d50f5736ba00679d9c24
wa1id <wa1id@users.noreply.github.com>
2020-06-14 21:16:30 +0200

    Add basic routing

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -7,12 +7,19 @@ import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <div className="content">
+          <Route path="/products" component={Products}></Route>
+          <Route path="/posts" component={Posts}></Route>
+          <Route path="/admin" component={Dashboard}></Route>
+          <Route path="/" component={Home}></Route>
+        </div>
       </div>
     );
   }
