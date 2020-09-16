226cdc61464e61d9a5089ca021986d958477302d
Walid <walid@Walids-iMac.local>
2020-09-17 00:54:00 +0200

    Handling form submission

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault(); //prevent default submit of form, no full page reload
+
+    //Call the server
+    console.log('submitted')
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
--- vidly/src/components/navbar.jsx
+++ vidly/src/components/navbar.jsx
@@ -3,15 +3,15 @@ import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-      <Link class="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+      <Link className="navbar-brand" to="/">
         Vidly
       </Link>
-      <button class="navbar-toggler" type="button">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse" id="navbarNav">
         <div className="navbar-nav">
           <NavLink className="nav-item nav-link" to="/movies">
             Movies
